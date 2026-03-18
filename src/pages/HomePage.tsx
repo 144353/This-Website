f7d9603f3bb3fd3dcfc47f3337c59048abd1573b
Qiu Jie <qj20050505@gmail.com>
2026-03-18 09:09:37 +0800

    added commit chart

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import { Documents } from '../components/Documents'
 import { Hero } from '../components/hero'
 import { WorkExperience } from '../components/WorkExperience'
 import { Projects } from './Projects'
+import GitHubChart from "../components/Git";
 
 export function HomePage() {
   return (
@@ -11,6 +12,7 @@ export function HomePage() {
       <About />
       <Documents />
       <WorkExperience />
+      <GitHubChart username="144353" />
       <Projects />
     </main>
   )
